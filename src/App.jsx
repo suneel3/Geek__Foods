2146c386a5ab102c7a4868c9babc8e70e7bdcb62
Suneel Kumar <shrikantsinghkashmeawahi@gmail.com>
2024-06-18 23:39:54 +0530

    First and last commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+
+import React from 'react'
+import Header from "./Header"
+import Footer from "./Footer"
+import "./App.css"
+import Menu from './Menu'
+
+function App() {
+  return (
+    <>
+     <Header />
+      <Menu />
+     <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/restaurant.js
@@ -0,0 +1,142 @@
+export const restaurants = [
+    {
+        name: "Comfort Diner",
+        cuisine: "American",
+        location: "New York, NY",
+        rating: 4.5,
+        image: "https://images.unsplash.com/photo-1548696060-8fae845c6452?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Mediterranean Grill",
+        cuisine: "Mediterranean",
+        location: "Austin, TX",
+        rating: 4.6,
+        image: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38"
+    },
+    {
+        name: "Taco Time",
+        cuisine: "Mexican",
+        location: "Austin, TX",
+        rating: 4.5,
+        image: "https://images.unsplash.com/photo-1546069901-eacef0df6022"
+    },
+    {
+        name: "Pizza Paradise",
+        cuisine: "Italian",
+        location: "Las Vegas, NV",
+        rating: 4.3,
+        image: "https://images.unsplash.com/photo-1528137871618-79d2761e3fd5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Spice Symphony",
+        cuisine: "Indian",
+        location: "Chicago, IL",
+        rating: 4.6,
+        image: "https://plus.unsplash.com/premium_photo-1666919058348-f64a57f83f61?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Tandoori Treasures",
+        cuisine: "Indian",
+        location: "Houston, TX",
+        rating: 4.6,
+        image: "https://images.unsplash.com/photo-1628294895950-9805252327bc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Sushi Delight",
+        cuisine: "Japanese",
+        location: "San Francisco, CA",
+        rating: 4.8,
+        image: "https://images.unsplash.com/photo-1553621042-f6e147245754"
+    },
+    {
+        name: "Taste of Thailand",
+        cuisine: "Thai",
+        location: "Denver, CO",
+        rating: 4.7,
+        image: "https://plus.unsplash.com/premium_photo-1669150852121-19bab9ca75f7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Taco Fiesta",
+        cuisine: "Mexican",
+        location: "Los Angeles, CA",
+        rating: 4.4,
+        image: "https://plus.unsplash.com/premium_photo-1698500045534-d2fa32f9929b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Burger Barn",
+        cuisine: "American",
+        location: "New York, NY",
+        rating: 4.4,
+        image: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38"
+    },
+    {
+        name: "Pho Haven",
+        cuisine: "Vietnamese",
+        location: "San Jose, CA",
+        rating: 4.8,
+        image: "https://plus.unsplash.com/premium_photo-1694715915615-51472fccf407?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Taste of Italy",
+        cuisine: "Italian",
+        location: "New York, NY",
+        rating: 4.8,
+        image: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092"
+    },
+    {
+        name: "Burger Bliss",
+        cuisine: "American",
+        location: "Phoenix, AZ",
+        rating: 4.4,
+        image: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90"
+    },
+    {
+        name: "BBQ Bliss",
+        cuisine: "BBQ",
+        location: "Memphis, TN",
+        rating: 4.8,
+        image: "https://images.unsplash.com/photo-1502998070258-dc1338445ac2?q=80&w=1958&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Ramen Rhapsody",
+        cuisine: "Japanese",
+        location: "Los Angeles, CA",
+        rating: 4.7,
+        image: "https://plus.unsplash.com/premium_photo-1692309186544-676236cdab1b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Ocean's Catch",
+        cuisine: "Seafood",
+        location: "San Diego, CA",
+        rating: 4.7,
+        image: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38"
+    },
+    {
+        name: "Veggie Heaven",
+        cuisine: "Vegetarian",
+        location: "Portland, OR",
+        rating: 4.9,
+        image: "https://plus.unsplash.com/premium_photo-1664205765643-9aec7ae06a2b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "French Finesse",
+        cuisine: "French",
+        location: "New York, NY",
+        rating: 4.6,
+        image: "https://images.unsplash.com/photo-1485962398705-ef6a13c41e8f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Steakhouse Supreme",
+        cuisine: "Steakhouse",
+        location: "Dallas, TX",
+        rating: 4.6,
+        image: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38"
+    },
+    {
+        name: "Fancy French",
+        cuisine: "French",
+        location: "Chicago, IL",
+        rating: 4.6,
+        image: "https://images.unsplash.com/photo-1485962398705-ef6a13c41e8f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+];
